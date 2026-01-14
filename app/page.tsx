5016ff76cdba4b31b27c01c59fa02942a09bc580
LuckyRandomness <hammel.k@northeastern.edu>
2026-01-14 17:54:30 -0500

    Labs 1-3 templates, updated main page to link

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,18 @@ import Link from "next/link";
 export default function Home() {
   return (
     <div>
-      <h1>Welcome to Web Dev!</h1>
-      <Link href="./labs/lab1">Lab 1 - HTML</Link>
+      <h1>Labs</h1>
+      <ul>
+        <li>
+          <Link href="./labs/lab1">Lab 1: HTML Examples</Link>
+        </li>
+        <li>
+          <Link href="./labs/lab2">Lab 2: CSS Basics</Link>
+        </li>
+        <li>
+          <Link href="./labs/lab3">Lab 3: Javascript Fundamentals</Link>
+        </li>
+      </ul>
     </div>
   );
 }
