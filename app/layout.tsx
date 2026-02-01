227f0ffbb21bf95bb47875268713beabe75509c6
LuckyRandomness <hammel.k@northeastern.edu>
2026-02-01 03:09:55 -0500

    vercal not deploying. changed app/layout trying to fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,20 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 // import "./globals.css";
-import "bootstrap/dist/css/bootstrap.min.css";
+//import "bootstrap/dist/css/bootstrap.min.css";
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {/* Layout UI */}
+        {/* Place children where you want to render a page or nested layout */}
+        <main>{children}</main>
+      </body>
+    </html>
+  )
+}
