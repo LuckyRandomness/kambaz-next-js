c36f09810b77375c4a30ed9f255d43234ba218e5
LuckyRandomness <hammel.k@northeastern.edu>
2026-02-17 20:11:59 -0500

    up to Breadcrumb

---
+++ app/(kambaz)/courses/[cid]/Breadcrumb.tsx
@@ -0,0 +1,19 @@
+"use client";
+import { usePathname } from "next/navigation";
+
+function capitalizeFirstLetter(val : string | undefined) {
+    if (val === undefined) {
+        return "";
+    } else {
+        console.log(val.charAt(0).toUpperCase() + val.slice(1));
+        return val.charAt(0).toUpperCase() + val.slice(1);
+    }
+}
+
+export default function Breadcrumb({ course }: { course: { name: string } | undefined; }) {
+ const pathname = usePathname();
+ return (
+   <span>
+     Course {course?.name} &gt; {capitalizeFirstLetter(pathname.split("/").pop())}
+   </span>
+);}
--- app/(kambaz)/dashboard/page.tsx
+++ app/(kambaz)/dashboard/page.tsx
@@ -1,104 +1,30 @@
 import Link from "next/link";
-import Image from "next/image";
+import * as db from "../database";
 import { Button, Card, CardBody, CardImg, CardText, CardTitle, Col, Row } from "react-bootstrap";
 export default function Dashboard(){
+    const courses = db.courses;
     return(
         <div id="wd-dashboard">
             <h1 id="wd-dashboard-title">Dashboard</h1> <hr />
-            <h2 id="wd-dashboard-published">Published Courses (7)</h2> <hr />
+            <h2 id="wd-dashboard-published">Published Courses ({courses.length})</h2> <hr />
             <div className="wd-dashboard-courses">
                 <Row xs={1} md={5} className="g-4">
-                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-                        <Card>
-                            <Link href="/courses/Programming" className="wd-dashboard-course-link text-decoration-none text-dark">
-                                <CardImg variant="top" src="/images/programmingicon.png" width="100%" height={160} alt="programming" />
-                                    <CardBody>
-                                        <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1234 Programming</CardTitle>
+                    {courses.map((course) => (
+                        <Col className="wd-dashboard-course" style={{ width: "300px" }}>
+                            <Card>
+                                <Link href={`/courses/${course._id}/home`} 
+                                className="wd-dashboard-course-link text-decoration-none text-dark">
+                                    <CardImg src="/images/programmingicon.png" variant="top" width="100%" height={160} />
+                                    <CardBody className="card-body">
+                                        <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">{course.name}</CardTitle>
                                         <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                                            Full Stack Software Development</CardText>
+                                            {course.description}</CardText>
                                         <Button variant="primary">Go</Button>
                                     </CardBody>
-                            </Link>
-                        </Card>
-                    </Col>
-                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-                        <Card>
-                            <Link href="/courses/Physics" className="wd-dashboard-course-link text-decoration-none text-dark">
-                                <CardImg variant="top" src="/images/physicsicon.png" width="100%" height={160} alt="phsyics" />
-                                    <CardBody>
-                                        <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS5678 Physics</CardTitle>
-                                        <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                                            AP Physics Mechanics</CardText>
-                                        <Button variant="primary">Go</Button>
-                                    </CardBody>
-                            </Link>
-                        </Card>
-                    </Col>
-                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-                        <Card>
-                            <Link href="/courses/Calculus" className="wd-dashboard-course-link text-decoration-none text-dark">
-                                <CardImg variant="top" src="/images/calculusicon.png" width="100%" height={160} alt="calculus" />
-                                    <CardBody>
-                                        <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS9012 Calculus</CardTitle>
-                                        <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                                            AP Calc BC</CardText>
-                                        <Button variant="primary">Go</Button>
-                                    </CardBody>
-                            </Link>
-                        </Card>
-                    </Col>
-                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-                        <Card>
-                            <Link href="/courses/Social Studies" className="wd-dashboard-course-link text-decoration-none text-dark">
-                                <CardImg variant="top" src="/images/socialstudiesicon.png" width="100%" height={160} alt="social studies" />
-                                    <CardBody>
-                                        <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS3456 Social Studies</CardTitle>
-                                        <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                                            Anthropology</CardText>
-                                        <Button variant="primary">Go</Button>
-                                    </CardBody>
-                            </Link>
-                        </Card>
-                    </Col>
-                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-                        <Card>
-                            <Link href="/courses/History" className="wd-dashboard-course-link text-decoration-none text-dark">
-                                <CardImg variant="top" src="/images/historyicon.png" width="100%" height={160} alt="history" />
-                                    <CardBody>
-                                        <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS7890 History</CardTitle>
-                                        <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                                            AP US History</CardText>
-                                        <Button variant="primary">Go</Button>
-                                    </CardBody>
-                            </Link>
-                        </Card>
-                    </Col>
-                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-                        <Card>
-                            <Link href="/courses/Art" className="wd-dashboard-course-link text-decoration-none text-dark">
-                                <CardImg variant="top" src="/images/articon.jpg" width="100%" height={160} alt="art" />
-                                    <CardBody>
-                                        <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS1245 Art</CardTitle>
-                                        <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                                            AP Fine Art</CardText>
-                                        <Button variant="primary">Go</Button>
-                                    </CardBody>
-                            </Link>
-                        </Card>
-                    </Col>
-                    <Col className="wd-dashboard-course" style={{ width: "300px" }}>
-                        <Card>
-                            <Link href="/courses/Music" className="wd-dashboard-course-link text-decoration-none text-dark">
-                                <CardImg variant="top" src="/images/musicicon.jpeg" width="100%" height={160} alt="music" />
-                                    <CardBody>
-                                        <CardTitle className="wd-dashboard-course-title text-nowrap overflow-hidden">CS6789 Music</CardTitle>
-                                        <CardText className="wd-dashboard-course-description overflow-hidden" style={{ height: "100px" }}>
-                                            AP Music</CardText>
-                                        <Button variant="primary">Go</Button>
-                                    </CardBody>
-                            </Link>
-                        </Card>
-                    </Col>
+                                </Link>
+                            </Card>
+                        </Col>
+                    ))}
                 </Row>
             </div>
         </div>
