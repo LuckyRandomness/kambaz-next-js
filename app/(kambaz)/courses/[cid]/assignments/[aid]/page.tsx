460df6e05b6c55a3da411c074b858655ad33dfd4
LuckyRandomness <hammel.k@northeastern.edu>
2026-03-07 15:48:12 -0500

    changed folder locations back

--- app/(kambaz)/courses/[cid]/assignments/[aid]/page.tsx
+++ app/(kambaz)/courses/[cid]/assignments/[aid]/page.tsx
@@ -1,44 +1,90 @@
-"use client";
-import Link from "next/link";
-import AssignmentsControls from "./AssignmentsControls";
-import { ListGroup, ListGroupItem } from "react-bootstrap";
-import { BsGripVertical } from "react-icons/bs";
-import { FaCaretDown } from "react-icons/fa6";
-import AssignmentControlButtons from "./AssignmentControlButtons";
-import LessonControlButtons from "../../modules/LessonControlButtons";
-import EntryButtonsLeft from "./EntryButtonsLeft";
+'use client'
+import { Button, Col, FormCheck, FormControl, FormLabel, FormSelect, Row } from "react-bootstrap";
+import Form from 'react-bootstrap/Form';
+import * as db from "../../../../database";
 import { useParams } from "next/navigation";
-import { addAssignment, editAssignment, updateAssignment, deleteAssignment } from "../reducer";
-import { useSelector, useDispatch } from "react-redux";
-import { RootState } from "@/app/(kambaz)/store";
-import { useState } from "react";
+import Link from "next/link";
 
-export default function Assignments() {
+export default function AssignmentEditor() {
     const { cid, aid } = useParams();
-    const [assignmentName, setAssignmentName] = useState("");
-    const { assignments } = useSelector((state: RootState) => state.assignmentReducer);
-    const dispatch = useDispatch();
+    const asgn = db.assignments.find((asg) => (asg._id === aid));
     return(
-        <div>
-            <AssignmentsControls /> <br /><br /><br /><br />
-            <ListGroup className="rounded-0 w-auto" id="wd-assignments">
-                <ListGroupItem className="wd-assignment p-0 mb-5 fs-5 border-gray">
-                    <div className="wd-title p-3 ps-2 bg-secondary fs-6">
-                        <BsGripVertical className="fs-3" /> <FaCaretDown className="me-2" /> 
-                        <b>ASSIGNMENTS</b> <AssignmentControlButtons />
-                    </div>
-                    <ListGroup id="wd-assignment-entries">
-                        {assignments
-                            .filter((asgn: any) => (asgn.course === cid))
-                            .map((asgn) => (
-                                <ListGroupItem className="wd-assignment-entry p-3 ps-1">
-                                    <div className="d-flex flex-row justify-content-between align-items-center"> 
-                                        <EntryButtonsLeft />
-                                        <div className="flex-fill ps-3">
-                                            <Link href={`/courses/${cid}/assignments/${asgn._id}`} className="wd-assignment-link fs-5 text-black text-decoration-none bold">
-                                                <b>{asgn.title}</b></Link><br />
-                                                <div className="fs-6"><span className="text-danger"> Multiple Modules </span> | <b>Not available until</b> May 6 at 12:00am | <br />
-                                                <b>Due</b> May 13 at 11:59pm | 100 pts</div></div>
-                                        <LessonControlButtons assignmentId={asgn._id} deleteAssignment={(assignmentId) => dispatch(deleteAssignment(assignmentId))} />
-</div> </ListGroupItem> ))} </ListGroup> </ListGroupItem> </ListGroup> </div>);
+        <div id="wd-assignments-editor">
+            <Form>
+                <Form.Group>
+                    <FormLabel htmlFor="wd-name">Assignment Name</FormLabel><br />
+                    <FormControl id="wd-name" defaultValue={asgn?.title} /><br />
+                    <FormControl as="textarea" rows={5} defaultValue="The assignment is available online. 
+                        Submit a link to the landing page of your Web application running on Netlify.
+                        The landing page should include the following: Your full name and section Links to each of the lab assignments
+                        Links to each of the lab assignments
+                        Links to the Kanbas application
+                        Links to all relevant source code repositories
+                The Kanbas application should include a link to navigate back to the landing page."/><br /><br />
+                </Form.Group>
+                    <Row className="justify-content-center">
+                        <Col className="text-end"><FormLabel htmlFor="wd-points">Points</FormLabel></Col>
+                        <Col><FormControl id="wd-points" defaultValue="100"/></Col>
+                    </Row> <br />
+                    <Row className="justify-content-center">
+                        <Col className="text-end"><FormLabel htmlFor="wd-assignment-group">Assignment Group</FormLabel></Col>
+                        <Col><FormSelect id="wd-assignment-group">
+                            <option value="ASSIGNMENTS">ASSIGNMENTS</option>
+                            <option value="QUIZZES">QUIZZES</option>
+                        </FormSelect> </Col>
+                    </Row> <br />
+                    <Row className="justify-content-center">
+                        <Col className="text-end"><FormLabel htmlFor="wd-display-grade-as">Display Grade as</FormLabel></Col>
+                        <Col><FormSelect id="wd-display-grade-as">
+                            <option value="PERCENTAGE">Percentage</option>
+                            <option value="FRACTION">Fraction</option>
+                        </FormSelect></Col>
+                    </Row><br />
+                    <Row className="justify-content-center">
+                        <Col className="text-end"><FormLabel htmlFor="wd-submission-type">Submission Type</FormLabel></Col>
+                        <Col className="border p-1 rounded">
+                            <FormSelect id="wd-submission-type">
+                                <option value="ONLINE">ONLINE</option>
+                                <option value="OFFLINE">OFFLINE</option>
+                            </FormSelect> <br />
+                            <FormCheck id="wd-chkbox-text-entry" label="Text Entry" name="online-entry-options"/> <br />
+                            <FormCheck id="wd-chkbox-website-url" label="Website URL" name="online-entry-options"/> <br />
+                            <FormCheck id="wd-chkbox-media-recordings" label="Media Recordings" name="online-entry-options"/> <br />
+                            <FormCheck id="wd-chkbox-student-annotations" label="Student Annotations" name="online-entry-options"/> <br />
+                            <FormCheck id="wd-chkbox-file-uploads" label="File Uploads" name="online-entry-options"/> <br />
+                        </Col>
+                    </Row><br />
+                    <Row className="justify-content-center">
+                        <Col className="text-end"><FormLabel htmlFor="wd-submission-type">Assign</FormLabel></Col>
+                        <Col className="border p-1 rounded">
+                            <Form.Group>
+                                <FormLabel><b>Assign to</b></FormLabel>
+                                <FormControl id="wd-assign" defaultValue="Everyone"/>
+                            </Form.Group> <br />
+                            <Form.Group>
+                                <FormLabel><b>Due</b></FormLabel>
+                                <FormControl type="datetime-local" value="2024-05-13T23:59"/>
+                            </Form.Group> <br />
+                            <div className="d-flex flex-row">
+                                <Form.Group>
+                                    <FormLabel><b>Available from</b></FormLabel>
+                                    <FormControl type="datetime-local" value="2024-05-06T00:00"/>
+                                </Form.Group> <br />
+                                <Form.Group>
+                                    <FormLabel><b>Until</b></FormLabel>
+                                    <FormControl type="datetime-local" />
+                                </Form.Group> <br />
+                            </div>
+                        </Col>
+                    </Row><br />
+            </Form>
+            <hr/>
+            <div className="d-flex flex-row justify-content-end">
+                <Link href={`/courses/${ cid }/assignments`}>
+                    <Button variant="secondary" size="lg" className="me-1 float-end"> Cancel </Button>
+                    <Button variant="danger" size="lg" className="me-1 float-end"> Save </Button>
+                </Link>
+            </div> 
+        </div>
+    );
 }
---
+++ app/(kambaz)/courses/[cid]/assignments/page.tsx
@@ -0,0 +1,44 @@
+"use client";
+import Link from "next/link";
+import AssignmentsControls from "./[aid]/AssignmentsControls";
+import { ListGroup, ListGroupItem } from "react-bootstrap";
+import { BsGripVertical } from "react-icons/bs";
+import { FaCaretDown } from "react-icons/fa6";
+import AssignmentControlButtons from "./[aid]/AssignmentControlButtons";
+import LessonControlButtons from "../modules/LessonControlButtons";
+import EntryButtonsLeft from "./[aid]/EntryButtonsLeft";
+import { useParams } from "next/navigation";
+import { addAssignment, editAssignment, updateAssignment, deleteAssignment } from "./reducer";
+import { useSelector, useDispatch } from "react-redux";
+import { RootState } from "@/app/(kambaz)/store";
+import { useState } from "react";
+
+export default function Assignments() {
+    const { cid } = useParams();
+    const [assignmentName, setAssignmentName] = useState("");
+    const { assignments } = useSelector((state: RootState) => state.assignmentReducer);
+    const dispatch = useDispatch();
+    return(
+        <div>
+            <AssignmentsControls /> <br /><br /><br /><br />
+            <ListGroup className="rounded-0 w-auto" id="wd-assignments">
+                <ListGroupItem className="wd-assignment p-0 mb-5 fs-5 border-gray">
+                    <div className="wd-title p-3 ps-2 bg-secondary fs-6">
+                        <BsGripVertical className="fs-3" /> <FaCaretDown className="me-2" /> 
+                        <b>ASSIGNMENTS</b> <AssignmentControlButtons />
+                    </div>
+                    <ListGroup id="wd-assignment-entries">
+                        {assignments
+                            .filter((asgn: any) => (asgn.course === cid))
+                            .map((asgn) => (
+                                <ListGroupItem className="wd-assignment-entry p-3 ps-1">
+                                    <div className="d-flex flex-row justify-content-between align-items-center"> 
+                                        <EntryButtonsLeft />
+                                        <div className="flex-fill ps-3">
+                                            <Link href={`/courses/${cid}/assignments/${asgn._id}`} className="wd-assignment-link fs-5 text-black text-decoration-none bold">
+                                                <b>{asgn.title}</b></Link><br />
+                                                <div className="fs-6"><span className="text-danger"> Multiple Modules </span> | <b>Not available until</b> May 6 at 12:00am | <br />
+                                                <b>Due</b> May 13 at 11:59pm | 100 pts</div></div>
+                                        <LessonControlButtons assignmentId={asgn._id} deleteAssignment={(assignmentId) => dispatch(deleteAssignment(assignmentId))} />
+</div> </ListGroupItem> ))} </ListGroup> </ListGroupItem> </ListGroup> </div>);
+}
