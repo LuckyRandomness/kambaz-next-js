f99a6f5fc58dd5a9724ac0e1a4496078940c8dca
LuckyRandomness <hammel.k@northeastern.edu>
2026-03-10 19:30:19 -0400

    adding addredux back

--- app/labs/lab4/redux/page.tsx
+++ app/labs/lab4/redux/page.tsx
@@ -6,8 +6,8 @@ export default function ReduxExamples() {
  return (
    <div>
      <h2>Redux Examples</h2>
-     {/*<AddRedux />
-     <CounterRedux />*/}
+     <AddRedux />
+     {/*<CounterRedux />*/}
    </div>
  );
 }
