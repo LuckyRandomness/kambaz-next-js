29ffd610d051b5b122180be9d72fd83b26e391f0
LuckyRandomness <hammel.k@northeastern.edu>
2026-03-10 16:17:36 -0400

    reducer

--- app/labs/lab4/store.ts
+++ app/labs/lab4/store.ts
@@ -2,7 +2,7 @@ import { configureStore } from "@reduxjs/toolkit";
 import helloReducer from "./redux/hello/helloReducer";
 import counterReducer from "./redux/CounterRedux/counterReducer";
 import addReducer from "./redux/AddRedux/addReducer";
-import todosReducer from "./redux/todos/todoReducer";
+import todosReducer from "./redux/todos/todosReducer";
 const store = configureStore({
     reducer: { helloReducer, 
                 counterReducer,
