f678c034f27ca6257c1e8ca62221ab89018a0cb1
LuckyRandomness <hammel.k@northeastern.edu>
2026-03-11 12:13:24 -0400

    fully deployable

--- app/labs/lab4/redux/page.tsx
+++ app/labs/lab4/redux/page.tsx
@@ -7,9 +7,9 @@ export default function ReduxExamples() {
  return (
    <div>
      <h2>Redux Examples</h2>
-     <HelloRedux />
+     {/*<HelloRedux />
      <CounterRedux />
-     <AddRedux />
+     <AddRedux />*/}
    </div>
  );
 }
