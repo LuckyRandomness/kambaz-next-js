c523b0827f90ccf2e749955f94aa0eaa30b90327
LuckyRandomness <hammel.k@northeastern.edu>
2026-03-11 11:54:44 -0400

    changed add/counter redux import

--- app/labs/lab4/redux/page.tsx
+++ app/labs/lab4/redux/page.tsx
@@ -1,13 +1,13 @@
 "use client";
-import AddRedux from "./AddRedux";
-import CounterRedux from "./CounterRedux";
+import AddRedux from "./AddRedux/index";
+import CounterRedux from "./CounterRedux/index";
 
 export default function ReduxExamples() {
  return (
    <div>
      <h2>Redux Examples</h2>
-     {/*<AddRedux />
-     <CounterRedux />*/}
+     <AddRedux />
+     <CounterRedux />
    </div>
  );
 }
